e3f289d6883c236814bf1a78a3cdd89517752639
Renato Valer <valer9826@gmail.com>
2025-04-03 02:08:35 -0500

    Add DetailFragment layout Add String resources Update main graph

--- app/src/main/java/com/renatovaler/globantchallenge/presentation/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/renatovaler/globantchallenge/presentation/ui/detail/DetailFragment.kt
@@ -5,15 +5,57 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import coil.load
 import com.renatovaler.globantchallenge.R
+import com.renatovaler.globantchallenge.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+
+    private lateinit var _binding: FragmentDetailBinding
+    private val binding get() = _binding
+
+    private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpUi()
+    }
+
+    private fun setUpUi() {
+        val country = args.country
+
+        with(binding) {
+            tvOfficialName.text = country.officialName
+            tvCommonName.text = country.commonName
+            tvCapital.text = country.capital
+            tvRegion.text = country.region
+            tvSubregion.text = country.subregion
+            tvPopulation.text = country.population
+            tvLanguages.text = country.languages
+            tvCurrencies.text = country.currencies
+            tvCarSide.text = country.carSide
+
+            imageFlag.load(country.flagUrl) {
+                crossfade(true)
+//                placeholder(R.drawable.placeholder_flag)
+//                error(R.drawable.error_flag)
+            }
+
+            imageCoatOfArms.load(country.coatOfArmsUrl) {
+                crossfade(true)
+//                placeholder(R.drawable.placeholder_shield)
+//                error(R.drawable.error_shield)
+            }
+        }
     }
 
 }
