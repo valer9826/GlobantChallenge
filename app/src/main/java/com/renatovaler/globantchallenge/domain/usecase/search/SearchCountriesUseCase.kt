29dd7e65ee9eaa96fb5a46d3aac12eebd5792e77
Renato Valer <valer9826@gmail.com>
2025-04-13 19:30:38 -0500

    Remove use cases

--- app/src/main/java/com/renatovaler/globantchallenge/domain/usecase/search/SearchCountriesUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.renatovaler.globantchallenge.domain.usecase.search
-import com.renatovaler.globantchallenge.domain.model.Country
-import kotlinx.coroutines.flow.Flow
-
-interface SearchCountriesUseCase {
-    operator fun invoke(query: String): Flow<Result<List<Country>>>
-}
-
