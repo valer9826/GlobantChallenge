dbced8b0fc1e2dc7b753fd8e108a4867ebbf1317
Renato Valer <valer9826@gmail.com>
2025-04-13 19:31:00 -0500

    Add nulls in countryDto

--- app/src/main/java/com/renatovaler/globantchallenge/data/dto/CountryDto.kt
+++ app/src/main/java/com/renatovaler/globantchallenge/data/dto/CountryDto.kt
@@ -1,15 +1,15 @@
 package com.renatovaler.globantchallenge.data.dto
 
 data class CountryDto(
-    val name: NameDto?,
-    val capital: List<String>?,
-    val region: String?,
-    val subregion: String?,
-    val flags: FlagsDto?,
-    val coatOfArms: CoatOfArmsDto?,
-    val population: Long?,
-    val languages: Map<String, String>?,
-    val currencies: Map<String, CurrencyDto>?,
-    val car: CarDto?
+    val name: NameDto? = null,
+    val capital: List<String>? = null,
+    val region: String? = null,
+    val subregion: String? = null,
+    val flags: FlagsDto? = null,
+    val coatOfArms: CoatOfArmsDto? = null,
+    val population: Long? = null,
+    val languages: Map<String, String>? = null,
+    val currencies: Map<String, CurrencyDto>? = null,
+    val car: CarDto? = null
 )
 
